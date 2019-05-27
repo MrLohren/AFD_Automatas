cc6f558e43e80c0f95cd730e07898951b32c01c4
Leninpresley <30590482+Leninpresley@users.noreply.github.com>
2019-05-27 00:12:10 -0400

    Add files via upload
    se añadió transiciones para estados ya existentes (no nuevos)
    fix label que muestra la transición (estaba malo el ss >> int, se cambió por un int = stoi(ss.str()))
    se pusieron los cálculos comunes de transición fuera de las condiciones

--- AFD_Automatas/mainwindow.cpp
+++ AFD_Automatas/mainwindow.cpp
@@ -22,7 +22,7 @@ string var;
 int cant_estados = 1;
 int i,e_salida,e_llegada;
 QString campotexto;
-nodo *lista = NULL; //Se declara la lista sin elementos
+nodo *lista = NULL; //Lista sin elementos
 // Funciones //
 void appendToList(nodo **l, int num1, string a, int num2);
 
@@ -39,9 +39,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
-{
+void MainWindow::on_pushButton_clicked(){
     if(ui->comboBox->currentText() != NULL && ui->comboBox_2->currentText() != NULL){
+        //Si en el combobox derecho se selecciona nuevo estado
         if(ui->comboBox_2->currentText() == "Nuevo Estado"){
             cant_estados +=1;
             ss.str(string());
@@ -49,19 +49,28 @@ void MainWindow::on_pushButton_clicked()
             ui->comboBox->addItem(QString::fromStdString(ss.str()));
             ui->comboBox_2->addItem(QString::fromStdString(ss.str()));
             ui->listWidget->addItem(QString::fromStdString(ss.str()));
-            ss.str(string());
-            ss << "Transición Exitosa: "<< ui->comboBox->currentText().toStdString()<< "--("<< ui->lineEdit->text().toStdString()<< ")-->"<< "q"<<cant_estados-1;
-            ui->label2->setText(QString::fromStdString(ss.str()));
-            ui->label2->setVisible(true);
-            ss.str(string());
-            for(i=1;i<ui->comboBox->currentText().toStdString().length();i++){  //En este for se hace el int e_salida igual al valor seleccionado en el combobox izquierdo
-                ss<<ui->comboBox->currentText().toStdString()[i];
-            }
-            ss >> e_salida;
+            //aqui iba el ctrl x
             e_llegada = cant_estados-1;  //No es necesario obtener el valor desde el combobox derecho ya que es un estado nuevo
-            appendToList(&lista,e_salida,ui->lineEdit->text().toStdString(),e_llegada); //Añade un nodo a la lista
-
         }
+        //Si en el combobox derecho se selecciona un estado existente
+        else{
+            ss.str(string());
+            for(i=1;i<(ui->comboBox_2->currentText().toStdString().length());i++){  //En este for se hace e_llegada igual al valor seleccionado en el combobox derecho
+                ss<<(ui->comboBox_2->currentText().toStdString()[i]);
+            }
+            e_llegada=stoi(ss.str());
+        }
+        //Aplica para ambos tipos de transición
+        ss.str(string());
+        for(i=1;i<(ui->comboBox->currentText().toStdString().length());i++){  //En este for se hace e_salida igual al valor seleccionado en el combobox izquierdo
+            ss<<(ui->comboBox->currentText().toStdString()[i]);
+        }
+        e_salida=stoi(ss.str());
+        ss.str(string());
+        ss << "Transición Exitosa: q"<<e_salida<<"--("<<ui->lineEdit->text().toStdString()<<")-->q"<<e_llegada;
+        ui->label2->setText(QString::fromStdString(ss.str()));
+        ui->label2->setVisible(true);
+        appendToList(&lista,e_salida,ui->lineEdit->text().toStdString(),e_llegada); //Añade un nodo a la lista
     }
     else{
         ui->label2->setText("No ha rellenado todos los campos necesarios.");
