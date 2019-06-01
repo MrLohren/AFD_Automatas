7af8b5e3e0fa687c1387d9ddf24ecd7d112de78c
Leninpresley <30590482+Leninpresley@users.noreply.github.com>
2019-05-31 20:09:35 -0400

    Add files via upload
    ahí ta la wea, no webeo más

---
+++ AFD_Automatas_v1.0/mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_comboBox_activated(const QString &arg1);
+
+    void on_comboBox_2_activated(const QString &arg1);
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButtonEFinales_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_comboBox_2_currentTextChanged(const QString &arg1);
+
+    void on_comboBox_currentTextChanged(const QString &arg1);
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
